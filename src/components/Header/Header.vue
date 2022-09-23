fae0cbcbbb334fdc846600f7c35b7e77628b9384
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-09-23 16:00:43 -0300

    feat: create Header Component

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="logo">
+      <img :src="logo" alt="" />
+    </div>
+    <div class="links">
+      <div>SELECT</div>
+      <div>icon</div>
+    </div>
+  </div>
+</template>
+
+<script src="./script.ts"></script>
+<style src="./styles.css"></style>
